e674d5c89eeb000cf1f3ec9cad1a3bcc95b70b1b
Kamronnahar-Monisha <kamronnaharmonisha12@gmail.com>
2022-10-27 01:14:30 +0600

    search by id api added

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
 app.get('/courses',(req,res)=>{
     res.send(courses);
 })
+app.get('/courses/:id',(req,res)=>{
+    const id = req.params.id;
+    res.send(courses.find(course=>course.id==id));
+})
 
 app.listen(port, () => {
   console.log(`Example app listening ${port}`)

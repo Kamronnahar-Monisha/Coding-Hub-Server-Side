cb17bf4df6f8d288614810d224279b4ed6d5789a
Kamronnahar-Monisha <kamronnaharmonisha12@gmail.com>
2022-10-27 12:12:23 +0600

    comment added

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const app = express()
 app.use(cors())
 const port = process.env.PORT || 5000;
 
-
+//data are loaded from course.json file
 const courses = require('./data/courses.json');
 
+//Home api
 app.get('/', (req, res) => {
   res.send('Welcome to coding hub server')
 })
+
+//All courses api
 app.get('/courses',(req,res)=>{
     res.send(courses);
 })
+
+//course by id api
 app.get('/courses/:id',(req,res)=>{
     const id = req.params.id;
     res.send(courses.find(course=>course.id==id));
